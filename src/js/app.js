2bdfdd1d95d48afd96b75a928ae7d2563f4e7842
Debriks <deborah.robbiano@gmail.com>
2022-12-07 15:40:14 +0100

    Project page overlays / Nav corrections

--- src/js/app.js
+++ src/js/app.js
@@ -1,37 +1,85 @@
 // HAMBURGER MENU ANIMATION
 
 const btnHamburger = document.querySelector("#btnHamburger");
-
 const header = document.querySelector(".header");
+const nav = document.querySelector(".header__subnav");
+const rule = document.querySelector(".header__rule");
 
 btnHamburger.addEventListener("click", function () {
-  if (header.classList.contains("open")) {
-    header.classList.remove("open");
-  } else {
+  if (!header.classList.contains("open")) {
     header.classList.add("open");
+    nav.classList.add("visible");
+    rule.classList.add("open");
+  } else {
+    header.classList.remove("open");
+    nav.classList.remove("visible");
+    rule.classList.remove("open");
   }
 });
 
-// STICKY NAVBAR
+// PROJECTS PAGE OVERLAYS
 
-// let hero = document.querySelector(".hero");
-// let nav = document.querySelector(".header");
+const btnOverlay1 = document.getElementById("btnOverlay1");
+const overlay1 = document.querySelector(".overlay-1");
 
-// window.onscroll = function () {
-//   if (window.pageYOffset > hero.offsetHeight - nav.offsetHeight) {
-//     nav.classList.remove("bottom-nav");
-//     nav.classList.add("top-nav");
-//   }
-// };
+const btnOverlay2 = document.getElementById("btnOverlay2");
+const overlay2 = document.querySelector(".overlay-2");
 
-// ARROW LINKS ON HOVER
+const btnOverlay3 = document.getElementById("btnOverlay3");
+const btnOverlay4 = document.getElementById("btnOverlay4");
+const btnOverlay5 = document.getElementById("btnOverlay5");
+const btnOverlay6 = document.getElementById("btnOverlay6");
+const btnOverlay7 = document.getElementById("btnOverlay7");
+const btnOverlay8 = document.getElementById("btnOverlay8");
 
-// const linkIcon = document.getElementById("linkIcon");
+const btnOverlayClose = document.querySelectorAll(".btn-close-overlay");
 
-// function hover(linkIcon) {
-//   linkIcon.setAttribute("src", "/images/red-arrow-hover.png");
-// }
+btnOverlay1.addEventListener("click", function () {
+  overlay1.classList.add("visible");
+});
 
-// function unhover(linkIcon) {
-//   linkIcon.setAttribute("src", "/images/red-arrow.png");
-// }
+btnOverlay2.addEventListener("click", function () {
+  overlay2.classList.add("visible");
+});
+
+btnOverlay3.addEventListener("click", function () {
+  overlay1.classList.add("visible");
+});
+
+btnOverlay4.addEventListener("click", function () {
+  overlay2.classList.add("visible");
+});
+
+btnOverlay5.addEventListener("click", function () {
+  overlay1.classList.add("visible");
+});
+
+btnOverlay6.addEventListener("click", function () {
+  overlay2.classList.add("visible");
+});
+
+btnOverlay7.addEventListener("click", function () {
+  overlay1.classList.add("visible");
+});
+
+btnOverlay8.addEventListener("click", function () {
+  overlay2.classList.add("visible");
+});
+
+btnOverlayClose.forEach((button) => {
+  button.addEventListener("click", () => {
+    overlay1.classList.remove("visible");
+    overlay2.classList.remove("visible");
+  });
+});
+
+// SWIPER
+
+const swiper = new Swiper(".mySwiper", {
+  loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
